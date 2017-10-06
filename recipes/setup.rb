8d38435e0f9798c9e66f4995da72c8c9a73cd494
root <root@ip-172-31-29-81.ec2.internal>
2017-10-06 22:15:30 +0000

    initial workstation cookbook commit

---
+++ recipes/setup.rb
@@ -0,0 +1,20 @@
+package 'vim-enhanced' 
+
+package 'tree' do
+action :install
+end
+
+package 'ntp' do
+action :install
+end
+
+package 'git' do
+action :install
+end 
+
+
+file '/etc/motd' do 
+content 'Property of Anirudh'
+owner 'root'
+group 'root'
+end
